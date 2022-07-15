e2ea772a12ca4f62abe9c6b77f2490c8f39dac5e
Jacopo Tedeschi <77614941+jacopotdsc@users.noreply.github.com>
2022-07-16 00:17:33 +0200

    Delete error_handler_updated.h

--- error_handler_updated.h
+++
@@ -1,8 +0,0 @@
-//#pragma once
-
-#ifndef ERROR_HANDLER_H
-#define ERROR_HANDLER_H
-
-#define handle_error(number,msg)	do{	if(number < 0 || number == NULL){ perror(msg); exit(EXIT_FAILURE); } } while(0)
-
-#endif

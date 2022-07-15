cb6004449070633a9f6f70cb62a81ddac930a545
Jacopo Tedeschi <77614941+jacopotdsc@users.noreply.github.com>
2022-07-15 15:02:58 +0200

    Add files via upload

--- error_handler.h
+++ error_handler.h
@@ -3,6 +3,7 @@
 #ifndef ERROR_HANDLER_H
 #define ERROR_HANDLER_H
 
-#define handle_error(number,msg)	do{	if(number < 0){ perror(msg); exit(EXIT_FAILURE); } } while(0)
+#define handle_error(number,msg)	do{	if(number < 0 || number == NULL){ perror(msg); exit(EXIT_FAILURE); } } while(0)
+#define handle_error_signal(number,msg)	do{	if(number == -1 ){ perror(msg); } } while(0)
 
 #endif
